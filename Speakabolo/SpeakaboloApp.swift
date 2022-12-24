9e33f242101b9138c4c4cfde94dc381ca2395d5c
Aniruddha Shukla <aniruddha.shukla@hotmail.com>
2022-12-23 18:45:27 -0600

    Initial Commit

---
+++ Speakabolo/SpeakaboloApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpeakaboloApp.swift
+//  Speakabolo
+//
+//  Created by Aniruddha Shukla on 12/23/22.
+//
+
+import SwiftUI
+
+@main
+struct SpeakaboloApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
